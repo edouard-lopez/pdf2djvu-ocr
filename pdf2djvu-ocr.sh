082c479ef659c59ad27fb189edeb1a6e04adc968
Édouard Lopez <ed8@ed8-laptop.(none)>
2013-02-09 22:02:03 +0100

    Quality debug (don't use -rgb!), split into functions

--- pdf2djvu-ocr.sh
+++ pdf2djvu-ocr.sh
@@ -3,32 +3,67 @@
 #   Convert PDF file to DJVU+OCR
 #
 # USAGE
-#
+#   pdf2djvu-ocr
+#   pdf2djvu-ocr /path/or/pattern
 #
 # DEPENDENCIES
 #   pdfsandwich tesseract
 #
 
-pattern="${1:-"*.pdf"}/*.pdf"
+pattern="${@:-"./*.pdf"}"
+THREAD_COUNT=4
+RES=300
 
-# printf "pattern: %s\n" "$pattern"
-
-for f in $(eval echo "$pattern");
-do
-    focr="${f/.pdf/.pdf.ocr}"
-    fdjvu="${focr/.pdf.ocr/.djvu}"
-
-    # printf "OCR filename: %s\n" "$focr"
-    # printf "DJVU+OCR filename: %s\n" "$fdjvu"
+function addOcr2Pdf() {
+    f="$1"
+    fpdfocr="$2"
 
     # generates PDF with OCR text,
+    printf "[i] Adding OCR text #1\n"
     /usr/bin/pdfsandwich -sloppy_text \
-        -tesseract "$(which tesseract)" \
-        -tesso -l fra \
-        "$f" -o "$focr"
+        -lang fra \
+        -nthreads "$THREAD_COUNT" \
+        -resolution "$RES"x"$RES" \
+        -o "$fpdfocr" \
+        -quiet \
+    "$f"
+}
 
-    [[ -f "$focr" ]] && printf "Failed to create %s\n" "$focr"
+function convert2djvu() {
+    fpdfocr="$1"
+    fdjvu="$2"
 
     # creates DjVu files from PDF files
-    pdf2djvu -o "$fdjvu" "$focr"
-done
+    pdf2djvu \
+        --bg-subsample=6 \
+        -o "$fdjvu" \
+        --jobs="$THREAD_COUNT" \
+        --fg-colors=web \
+        --quiet \
+    "$fpdfocr"
+}
+
+
+function run() {
+    # printf "pattern: %s\n" "$(eval echo "$pattern")"
+
+    for f in $(eval echo "$pattern");
+    do
+        fpdfocr="${f/.pdf/.pdf.ocr}"
+        fdjvu="${fpdfocr/.pdf.ocr/.djvu}"
+
+        # printf "OCR filename: %s\n" "$fpdfocr"
+        # printf "DJVU+OCR filename: %s\n" "$fdjvu"
+
+        addOcr2Pdf "$f" "$fpdfocr"
+
+        if [[ -f "$fpdfocr" ]]; then
+            printf "Failed to create %s\n" "$fpdfocr"
+            continue
+        fi
+        convert2djvu "$fpdfocr" "$fdjvu"
+
+    done
+}
+
+time run; # start script
